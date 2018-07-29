adcccc11d9d11fd639de67efa12729e5e031e454
Smarpit Singh <smarpitsingh560@gmail.com>
2018-07-29 16:29:46 +0530

    Entity created

--- app/src/main/java/com/example/android/todoaac/AddTaskActivity.java
+++ app/src/main/java/com/example/android/todoaac/AddTaskActivity.java
@@ -1,19 +1,3 @@
-/*
-* Copyright (C) 2016 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-
 package com.example.android.todoaac;
 
 import android.content.Intent;
@@ -33,15 +17,16 @@ public class AddTaskActivity extends AppCompatActivity {
     public static final String EXTRA_TASK_ID = "extraTaskId";
     // Extra for the task ID to be received after rotation
     public static final String INSTANCE_TASK_ID = "instanceTaskId";
-    // Constants for priority
+
     public static final int PRIORITY_HIGH = 1;
     public static final int PRIORITY_MEDIUM = 2;
     public static final int PRIORITY_LOW = 3;
+
     // Constant for default task id to be used when not in update mode
     private static final int DEFAULT_TASK_ID = -1;
-    // Constant for logging
+
     private static final String TAG = AddTaskActivity.class.getSimpleName();
-    // Fields for views
+
     EditText mEditText;
     RadioGroup mRadioGroup;
     Button mButton;
@@ -73,9 +58,7 @@ public class AddTaskActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
     }
 
-    /**
-     * initViews is called from onCreate to init the member variable views
-     */
+
     private void initViews() {
         mEditText = findViewById(R.id.editTextTaskDescription);
         mRadioGroup = findViewById(R.id.radioGroup);
@@ -89,26 +72,17 @@ public class AddTaskActivity extends AppCompatActivity {
         });
     }
 
-    /**
-     * populateUI would be called to populate the UI when in update mode
-     *
-     * @param task the taskEntry to populate the UI
-     */
+
     private void populateUI(TaskEntry task) {
 
     }
 
-    /**
-     * onSaveButtonClicked is called when the "save" button is clicked.
-     * It retrieves user input and inserts that new task data into the underlying database.
-     */
+
     public void onSaveButtonClicked() {
         // Not yet implemented
     }
 
-    /**
-     * getPriority is called whenever the selected priority needs to be retrieved
-     */
+
     public int getPriorityFromViews() {
         int priority = 1;
         int checkedId = ((RadioGroup) findViewById(R.id.radioGroup)).getCheckedRadioButtonId();
@@ -125,11 +99,7 @@ public class AddTaskActivity extends AppCompatActivity {
         return priority;
     }
 
-    /**
-     * setPriority is called when we receive a task from MainActivity
-     *
-     * @param priority the priority value
-     */
+
     public void setPriorityInViews(int priority) {
         switch (priority) {
             case PRIORITY_HIGH:
--- app/src/main/java/com/example/android/todoaac/MainActivity.java
+++ app/src/main/java/com/example/android/todoaac/MainActivity.java
@@ -1,19 +1,3 @@
-/*
-* Copyright (C) 2016 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-
 package com.example.android.todoaac;
 
 import android.content.Intent;
@@ -31,9 +15,8 @@ import static android.support.v7.widget.DividerItemDecoration.VERTICAL;
 
 public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemClickListener {
 
-    // Constant for logging
     private static final String TAG = MainActivity.class.getSimpleName();
-    // Member variables for the adapter and RecyclerView
+
     private RecyclerView mRecyclerView;
     private TaskAdapter mAdapter;
 
@@ -42,25 +25,18 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Set the RecyclerView to its corresponding view
+
         mRecyclerView = findViewById(R.id.recyclerViewTasks);
 
-        // Set the layout for the RecyclerView to be a linear layout, which measures and
-        // positions items within a RecyclerView into a linear list
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        // Initialize the adapter and attach it to the RecyclerView
         mAdapter = new TaskAdapter(this, this);
         mRecyclerView.setAdapter(mAdapter);
 
         DividerItemDecoration decoration = new DividerItemDecoration(getApplicationContext(), VERTICAL);
         mRecyclerView.addItemDecoration(decoration);
 
-        /*
-         Add a touch helper to the RecyclerView to recognize when a user swipes to delete an item.
-         An ItemTouchHelper enables touch behavior (like swipe and move) on each ViewHolder,
-         and uses callbacks to signal when a user is performing these actions.
-         */
+
         new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
             @Override
             public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
@@ -74,17 +50,12 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
             }
         }).attachToRecyclerView(mRecyclerView);
 
-        /*
-         Set the Floating Action Button (FAB) to its corresponding View.
-         Attach an OnClickListener to it, so that when it's clicked, a new intent will be created
-         to launch the AddTaskActivity.
-         */
-        FloatingActionButton fabButton = findViewById(R.id.fab);
 
+        FloatingActionButton fabButton = findViewById(R.id.fab);
         fabButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Create a new intent to start an AddTaskActivity
+
                 Intent addTaskIntent = new Intent(MainActivity.this, AddTaskActivity.class);
                 startActivity(addTaskIntent);
             }
@@ -93,6 +64,6 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
 
     @Override
     public void onItemClickListener(int itemId) {
-        // Launch AddTaskActivity adding the itemId as an extra in the intent
+
     }
 }
